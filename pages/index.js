d89682e18adcad5a65efe374c3d20f0da0fc96b0
David Novak <david.novak@vacuumlabs.com>
2022-02-11 17:17:44 +0100

    Add base setup with nextjs & tailwind

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Institut digitalnych inovacii</title>
+        <meta name="description" content="Some description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      MAIN
+    </>
+  );
+}
