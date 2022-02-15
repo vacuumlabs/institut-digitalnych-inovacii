4f9b35087cba0d98c4baed9cab1fbbb71d67e5b1
David Novak <david.novak@vacuumlabs.com>
2022-02-15 09:56:45 +0100

    Add base og:data

---
+++ data/meta.js
@@ -0,0 +1,6 @@
+export const meta = {
+  title: "Inštitút digitálnych inovácií - Za inovatívne Slovensko",
+  description:
+    "Inštitút digitálnych inovácií je nezávislá občianska platforma, ktorá slúži na presadzovanie politík, ktoré sú kľúčové pre lepšie fungovanie inovačného ekosystému.",
+  imageUrl: "/assets/idi-og.jpg",
+};
--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,37 @@ import Goals from "../components/Goals";
 import Hero from "../components/Hero";
 import Board from "../components/Board";
 import Footer from "../components/Footer";
+import { meta } from "../data/meta";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Institut digitalnych inovacii</title>
-        <meta name="description" content="Some description" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
+        <meta property="og:image" content={meta.imageUrl} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicon/favicon-16x16.png"
+        />
+        <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#328BF2" />
+        <meta name="theme-color" content="#328BF2" />
       </Head>
       <Header />
       <main>
